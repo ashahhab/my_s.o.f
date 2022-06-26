d3859bca629ae018f8889f3b2c00190c4528fff3
shahab <shahhab2727@gmail.com>
2022-06-26 20:21:44 +0430

    finished reser password objects

--- account/views.py
+++ account/views.py
@@ -10,7 +10,7 @@ from home.models import Post
 from django.contrib.auth import views as auth_views
 from django.urls import reverse_lazy
 
-
+# user
 class UserRegisterView(View):
     form_class = UserRegistrationForm
     template_name = 'account/register.html'
@@ -66,15 +66,37 @@ class UserLogoutView(LoginRequiredMixin, View):
         logout(request)
         messages.success(request, 'you loged out successfully', 'success')
         return redirect('home:home')
-    
+
+
+
+
+#profile
 
 class UserProfileView(LoginRequiredMixin, View):
     def get(self, request, user_id):
         user = get_object_or_404(User, id=user_id)
         posts = Post.objects.filter(user = user)
         return render(request, 'account/profile.html', {'user':user, 'posts':posts})
-    
+
+
+
+
+# reset password
+
 class UserPasswordResetView(auth_views.PasswordResetView):
     template_name = 'account/password_reset_form.html'
     success_url = reverse_lazy('account/password_reset_done')
     email_template_name = 'account/password_reset_email.html'
+
+
+class UserPasswordResetDoneView(auth_views.PasswordResetDoneView):
+    template_name = 'account/password_reset_done.html'
+    
+
+class UserPasswordResetConfirmView(auth_views.PasswordResetConfirmView):
+    template_name = 'account/password_reset_confirm.html'
+    success_url = reverse_lazy('account:password_reset_complete')
+
+    
+class UserPasswordResetCompleteView(auth_views.PasswordResetCompleteView):
+    template_name = 'account/password_reset_complete.html'
